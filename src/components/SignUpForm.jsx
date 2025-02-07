579b3933b3305cc1e7a028241b8bc12d95347f01
Brianna Stephens <briannaastephens24@gmail.com>
2025-02-07 08:03:45 -0700

    second commit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -3,40 +3,76 @@ import { useState } from "react"
 
 const SignUpForm = () => {
 
-const [usernameInput, setUsernameInput] = useState(``)
-const [passwordInput, setPasswordInput] = useState(``)
-const [error, setError] = useState(`null`)
+  const [usernameInput, setUsernameInput] = useState(``)
+  const [passwordInput, setPasswordInput] = useState(``)
+  const [error, setError] = useState(null)
 
-const handleSubmit = async(event) => {
-event.preventDefault()
-console.log(`hello`)
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+
+  const user = {
+    username: usernameInput,
+    password: passwordInput,
+  };
+
+  try {
+  const response = await fetch (`https://fsa-jwt-practice.herokuapp.com/signup`, {
+    method:"POST", 
+    headers: {
+      "content-type":"application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  const result = await response.json();
+  console.log (result);
+
+  if (!response.ok) {
+    throw new Error (result.message `Oh noooo, an error occured when creating user!`)
+  }
+  console.log(`User created successfully! Yay!`, result);
+  setUsernameInput(``);
+  setPasswordInput(``);
+  setError(null);
+} catch (err) {
+  console.log(`Error happened oh nooo`,err.message);
+  setError(err.message);
 }
 
-return (
-<>
-<h2>Sign Up </h2>
+  }
 
-<form onSubmit={handleSubmit}>
 
-<input
-placeholder = "Username"
-onChange={ (event) => { setUsernameInput(event.target.value);console.log(usernameInput)
-}}
 
-/>
+  return (
+    <>
+      <h2>Sign Up </h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
 
-<input
-placeholder = "Password" 
-onChange = { (event) => { setPasswordInput(event.target.value)}}
-/>
+      <form onSubmit={handleSubmit}>
 
-<button>Log In!</button>
+        <input
+          placeholder="Username"
+          onChange={(event) => {
+            setUsernameInput(event.target.value); console.log(usernameInput)
+          }}
 
-</form>
-</>
+        />
 
-)
+        <input
+          placeholder="Password"
+          onChange={(event) => { setPasswordInput(event.target.value); console.log(passwordInput) }}
+        />
+
+        <button>Log In!</button>
+
+      </form>
+     
+  
+    </>
+
+  )
 
 }
 
-export default SignUpForm
+export default SignUpForm 
